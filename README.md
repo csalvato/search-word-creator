d93a4d777bd0cf40a3f7db0ceacc807b693fb91c
Chris Salvato <csalvato@gmail.com>
2012-10-31 11:11:44 +0000

    Initial Commit with README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+# Search Word Creator
+
+Search Word Creator lets users create search word puzzles quickly and easily for education (or fun!).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SearchWordCreator::Application.config.secret_token = '22d3d62cfe2cb12229a32c1eb2b333b2d3e6c4391ba97d406203497cbf60ffa9e14641c197c04e98093473dc28fb9862009336a4bed86797c58129ded9550b33'
