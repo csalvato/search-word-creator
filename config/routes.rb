70f7775b23f8d71c6c0b728fb55ca36b9ee599bc
Chris Salvato <csalvato@gmail.com>
2012-11-01 01:38:52 +0000

    Finish for night.  Break ground on static pages layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 SearchWordCreator::Application.routes.draw do
-  match '/contact', to: 'static_pages#contact'
   match '/faq', to: 'static_pages#faq'
-  match '/plans-and-pricing', to: 'static_pages#plans'
+  match "/contact", to: "static_pages#contact"
+  match "/plans-and-pricing" => "static_pages#plans", :as => "plans"
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
