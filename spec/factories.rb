8c454be55bcc5cc893542ba32eaabc84b1f1f93c
Chris Salvato <csalvato@gmail.com>
2012-11-16 14:21:04 +0000

    Create user model specs for BDD implementation.  Need to implement functionality to make initial specs pass

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    sequence(:name) { |n| "person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"} 
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
